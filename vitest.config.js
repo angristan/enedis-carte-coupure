77a3bc021f00d78e7fd9e4f33d5d315b9240f064
Stanislas Lange <git@slange.me>
2026-07-16 20:29:12 +0200

    Remove obsolete JavaScript configs

--- vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    include: ["worker/**/*.test.js", "railway-redirect/**/*.test.js"],
-    environment: "node",
-  },
-});
